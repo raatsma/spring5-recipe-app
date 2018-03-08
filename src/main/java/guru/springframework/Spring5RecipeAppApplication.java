08176beac3f3eb2b4a6de239ab3d622060bcb62c
ICONOS\raatsma <mathijs.raats@geosolutions.be>
2018-03-08 11:00:24 +0100

    Assignment review

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,12 +1,13 @@
 package guru.springframework;
 
+import guru.springframework.repositories.RecipeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(Spring5RecipeAppApplication.class, args);
 	}
 }
--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -12,6 +12,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -19,8 +22,15 @@ public class Ingredient {
         return id;
     }
 
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure uom;
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
 
     public void setId(Long id) {
         this.id = id;
